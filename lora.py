2a204fd5969c41f008c7f5cddb1dd9671e07aaf5
Andrew Wu <andrew.m.wu@gmail.com>
2024-03-29 15:09:52 -0500

    initial run.py setup for roberta

--- lora.py
+++ lora.py
@@ -2,20 +2,20 @@ import torch
 import torch.nn as nn
 import torch.functional as F
 
+#class LoRALayer(nn.Module):
+#    def __init__(self, in_dim, out_dim, rank, alpha):
+#        super().__init__()
+#        std_dev = 1 / torch.sqrt(torch.tensor(rank))
+#        self.rank = rank
+#        self.alpha = alpha
+#        self.A = nn.Parameter(torch.randn(in_dim, rank) * std_dev)
+#        self.B = nn.Parameter(torch.zeros(rank, out_dim))
+#    
+#    def forward(self, x):
+#        return self.alpha * (x @ self.A @ self.B)
+
 class LoRALayer(nn.Module):
-    def __init__(self, in_dim, out_dim, rank, alpha):
-        super().__init__()
-        std_dev = 1 / torch.sqrt(torch.tensor(rank))
-        self.rank = rank
-        self.alpha = alpha
-        self.A = nn.Parameter(torch.randn(in_dim, rank) * std_dev)
-        self.B = nn.Parameter(torch.zeros(rank, out_dim))
-    
-    def forward(self, x):
-        return self.alpha * (x @ self.A @ self.B)
-    
-class AlsoLoRALayer(nn.Module):
-    # the same as lora, just conceptually nicer imo
+    # the same as lora, just conceptually nicer imo, can also add bias
     def __init__(self, in_dim, out_dim, rank, alpha):
         super().__init__()
         self.rank = rank
@@ -29,9 +29,22 @@ class AlsoLoRALayer(nn.Module):
     
     def forward(self, x):
         return self.alpha * self.seq(x)
+    
+class LinearWithLoRA(torch.nn.Module):
+    # wrapper around Linear Layers for normal LoRA
+    def __init__(self, linear, rank, alpha):
+        super().__init__()
+        self.linear = linear
+        self.lora = LoRALayer(
+            linear.in_features, linear.out_features, rank, alpha
+        )
 
+    def forward(self, x):
+        return self.linear(x) + self.lora(x)
+    
 class ProgressiveLoRANet(nn.Module):
     # progressive net style - but only one connection vertically
+    # TODO: test and add diag connections
     def __init__(self, base_model, lora_rank, lora_alpha):
         super().__init__()
         self.base_model = base_model
@@ -53,19 +66,9 @@ class ProgressiveLoRANet(nn.Module):
             x = x + lora_layer(x)
         return x
     
-class LinearWithLoRA(torch.nn.Module):
-    def __init__(self, linear, rank, alpha):
-        super().__init__()
-        self.linear = linear
-        self.lora = LoRALayer(
-            linear.in_features, linear.out_features, rank, alpha
-        )
-
-    def forward(self, x):
-        return self.linear(x) + self.lora(x)
-    
-# Code inspired by https://github.com/catid/dora/blob/main/dora.py
 class LinearWithDoRA(torch.nn.Module):
+    # Code inspired by https://github.com/catid/dora/blob/main/dora.py
+    # from LightningAI (see 02_finetune-with-lora.ipynb)
     def __init__(self, linear, rank, alpha):
         super().__init__()
         self.linear = linear
