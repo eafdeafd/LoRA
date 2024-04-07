5d769d6b35e3b3281834a81e3a310a55b27e3050
Adeet Parikh <adpar2001@gmail.com>
2024-04-07 14:46:53 -0500

    sst helper

---
+++ dataset_helpers/sst.py
@@ -0,0 +1,12 @@
+def prepare_dataset_sst(examples, tokenizer, max_seq_length=None):
+    max_seq_length = tokenizer.model_max_length if max_seq_length is None else max_seq_length
+
+    tokenized_examples = tokenizer(
+        examples['sentence'],
+        truncation=True,
+        max_length=max_seq_length,
+        padding='max_length'
+    )
+
+    tokenized_examples['label'] = examples['label']
+    return tokenized_examples
